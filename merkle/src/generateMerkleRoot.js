0f1d74126185e9e727879f608d078c05a1e21b77
Hoang Trinh <hoang.trinhj@gmail.com>
2022-01-27 09:43:02 +0700

    feat: Generate Merkle Data

---
+++ merkle/src/generateMerkleRoot.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const fs = require('fs');
+const argv = require('yargs/yargs')(process.argv.slice(2)).argv;
+const { parseRewards } = require('./parseRewards');
+const configPath = argv.f;
+
+let json = JSON.parse(fs.readFileSync(path.join(__dirname, configPath), { encoding: 'utf8' }));
+let cycle = json['cycle'];
+if (typeof json !== 'object') {
+  throw new Error('Invalid JSON');
+}
+
+json = JSON.stringify(parseRewards(json), null, 2);
+fs.writeFileSync(path.join(__dirname, `../../results/cycle_${cycle}/merkle_data.json`), json);
+console.log(`Writing merkle data to results//cycle_${cycle}/merkle_data.json ... Done.`);
+fs.writeFileSync(path.join(__dirname, `../../results/latest_merkle_data.json`), json);
