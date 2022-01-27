44d54ba6ae2b2224e237617b96ea93fec4531d81
Hoang Trinh <hoang.trinhj@gmail.com>
2022-01-27 14:31:03 +0700

    chore: Update Script To Run

---
+++ compile_and_run.sh
@@ -0,0 +1,9 @@
+go build -o zkyber_reward &&
+./zkyber_reward fetcher -c pkg/config/ethereum.yaml &&
+./zkyber_reward fetcher -c pkg/config/polygon.yaml &&
+./zkyber_reward fetcher -c pkg/config/bsc.yaml &&
+./zkyber_reward fetcher -c pkg/config/avalanche.yaml &&
+./zkyber_reward fetcher -c pkg/config/fantom.yaml &&
+./zkyber_reward fetcher -c pkg/config/cronos.yaml &&
+./zkyber_reward reward --cycle 1 &&
+node merkle/src/generateMerkleRoot.js -f ../../results/cycle_1/reward_data.json
