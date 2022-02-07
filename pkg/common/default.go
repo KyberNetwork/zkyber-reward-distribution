b6a33d3c14323d2b5c604ee5e66fefc3a6c77278
Hoang Trinh <hoang.trinhj@gmail.com>
2022-02-07 10:22:54 +0700

    chore: Update Latest Data

--- pkg/common/default.go
+++ pkg/common/default.go
@@ -3,7 +3,7 @@ package common
 const (
 	DefaultPhaseId        = 1
 	DefaultStartTimestamp = 1640131200
-	DefaultEndTimestamp   = 1643587200
+	DefaultEndTimestamp   = 1643644799
 	DefaultTotalReward    = "30000000000000000000000"
 )
 
